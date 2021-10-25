f599e2ce65ca7942e6ecf5d72c7d510c85ff1739
s.senthilnathan <libertariansenthil@gmail.com>
2021-10-25 19:39:12 +0530

    version 0.1.0 skeleton

---
+++ matrix_project/matrix/__init__.py
@@ -0,0 +1,4 @@
+# Initializing a package named 'matrix' and including all other modules in this package so that it can be accessed easily.
+
+# Current version of the project.
+__version__ = "0.1.0" # Lastly updated on 25th oct 2021.
