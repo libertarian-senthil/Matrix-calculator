d47a44c13e1eb09a621b5602d45c6c65bd9348e7
s.senthilnathan <libertariansenthil@gmail.com>
2021-10-26 08:44:14 +0530

    changed mat_input,matrix() and removed cmd_ui.py

--- matrix_project/matrix/mat_input.py
+++ matrix_project/matrix/mat_input.py
@@ -1,17 +1,17 @@
 #Basic matrix input file for all arithmetic values
-def empt(ch):
+def matrix(ch):
     ''' matrix addition'''
     while True:
-        a = [[0,0], 
-            [0,0]
+        a = [[0,0, 0], 
+            [0,0, 0]
             ]
         try:
-            for i in range(2):
-                for j in range(2):
-                    e = int(input(f"{ch}:"))
+            for i in range(3):
+                for j in range(3):
+                    e = int(input(f"{ch}[{i}][{j}]: "))
                     a[i][j] = e
             return a
         except ValueError:
             print("You've given a incorrect input")
 if __name__ == '__main__':
-    print(empt('a'))
+    print(matrix('a'))
