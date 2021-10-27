2742234947fe299a6e468f0d8fb7207c5805cd38
s.senthilnathan <83577018+libertarian-senthil@users.noreply.github.com>
2021-10-27 20:48:22 +0530

    Name of  transpose is changed

--- matrix_project/matrix/formula.py
+++ matrix_project/matrix/formula.py
@@ -1,19 +1,24 @@
-#status:active
-#All the other formulas are done here
+# Status:active
+# All the other formulas are done here
+
 import numpy as np
 from mat_input import *
 
 class formula:
-    def __init__(self, arr_a = None, arr_b = None):
+    """ Preforms matrix oriented operations """
+    
+    def __init__(self, arr_a = None, arr_b = None):    # Constructor
         self.arr_a= np.array(arr_a)
         self.arr_b= np.array(arr_b)
         self.result= None
+        
     def  square(self, dim):
         """ Returns Square of two arrays of same dimension """
-        self.arr_a= matrix(dim, dim) # Error spot
+        self.arr_a= matrix(dim, dim)
         self.result= np.square(self.arr_a)
         return self.result
-    def trps(self, dim): 
+    
+    def trans(self, dim): 
         """ Returns Transpose of two arrays of same dimension """
         self.arr_a= np.matrix(dim, dim) # Error spot
         self.result= matrix.T(self.arr_a)
