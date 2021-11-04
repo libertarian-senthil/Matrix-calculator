148cba967a225b9a925c1c45a136f2bd3e6dd470
s.senthilnathan <libertariansenthil@gmail.com>
2021-11-04 20:48:23 +0530

    GUI Directory created Main_window.py initiated

---
+++ matrix_project/GUI/main_window.py
@@ -0,0 +1,27 @@
+# Status: active
+
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.title("Matrix calculator")
+root.geometry("950x650")
+root.resizable(False, False)
+root.columnconfigure(0, weight= 1)
+root.columnconfigure(1, weight= 1)
+root.columnconfigure(2, weight= 1)
+
+
+operations = ttk.Frame(root, border=3, relief=tk.SUNKEN)
+operations.grid(row= 0, column=0)
+
+part1= ttk.Frame(root, border=3, relief=tk.SUNKEN)
+part1.grid(row=0, column=1)
+
+l2= tk.Label(part1, text="A")
+l2.grid(row= 0, column= 0, padx= 2, pady= 2)
+
+l1= tk.Label(operations, text="A")
+l1.grid(row= 0, column= 0, padx= 2, pady= 2)
+
+root.mainloop()   
