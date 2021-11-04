a6c92384dd04a8a4f2064b79279b45b3910e78c0
s.senthilnathan <libertariansenthil@gmail.com>
2021-11-04 23:44:49 +0530

    Minor chaages

--- matrix_project/matrix/main.py
+++ matrix_project/matrix/main.py
@@ -28,27 +28,27 @@ while True:
 # We can give the No. of dimension needed in non-programming side
 if option == 1:
     print("addition") #test code
-    a= arithmetic()
-    print(a.add(eval(input("Enter the No. of dimensions:"))))
+    addition= arithmetic()
+    print(addition.add(eval(input("Enter the No. of dimensions:"))))
 elif option ==2:
     print("Subtraction") #test code
-    b = arithmetic()
-    print(b.sub(eval(input("Enter the No. of dimensions:"))))
+    subtraction = arithmetic()
+    print(subtraction.sub(eval(input("Enter the No. of dimensions:"))))
 elif option == 3:
     print("Multiplication") #test code
-    c = arithmetic()
-    print(c.multi(eval(input("Enter the No. of dimensions:")))) #Need to change for multi dimensions like 2*3 and 3*2, Yet to be completed
+    multiply= arithmetic()
+    print(multiply.multi(eval(input("Enter the No. of dimensions:")))) #Need to change for multi dimensions like 2*3 and 3*2, Yet to be completed
 elif option == 4:
     print("square of A") #test code
-    d = formula()
-    print(d.square(eval(input("Enter the No. of dimensions:"))))
+    division = formula()
+    print(division.square(eval(input("Enter the No. of dimensions:"))))
 elif option == 5:
     print("transpose")   # test code
     e = formula() 
-    print(e.transpose(2))
+    print(e.trans(2))
     print("transpose")
     e = formula()
-    print(e.transpose(eval(input("Enter the No. of dimensions:"))))
+    print(e.trans(eval(input("Enter the No. of dimensions:"))))
 elif option == 6:
     print("Exited") 
 else: 
