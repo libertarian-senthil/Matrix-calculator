6b03f9c576559c2189bda515b4ec4173e5a87358
ram <bornastraeus01@gmail.com>
2021-11-08 10:04:57 +0530

    added test file

---
+++ matrix_project/matrix/_test.py
@@ -0,0 +1,10 @@
+#status:active
+#For checking the upcoming any errors just for trail purpose
+import numpy as np
+array1 = np.array([1, 2, 3])
+array2 = np.array([[1, 2], [3, 4]])
+n = 5
+print(np.multiply(array1,n))
+print(np.multiply(array2,n))
+
+#yet to be completed
