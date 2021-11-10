afbbebbd5db4e5dae94b46d6ec5300f75d64f564
s.senthilnathan <libertariansenthil@gmail.com>
2021-11-10 17:46:28 +0530

    Docstring added according to PEP257

--- matrix_project/matrix/arithmetic.py
+++ matrix_project/matrix/arithmetic.py
@@ -1,33 +1,98 @@
 # Status: active
-# All the Arithmetic calculations are done here.
+"""
+Creating class arithmetic in order to  perform arithmetic operations 
+for matrices. the following operations are performed addtion, 
+subtration, and multiplication.
+
+classes
+-------
+arithmetic:
+    A class performing arithmetic operations on matrices.
+"""
 
 import numpy as np
+
 from mat_input import *
 
 class arithmetic:
-    """ Class returns the arithmetic operations for matrices """
+    """Return the arithmetic operations for matrices. 
     
-    def __init__(self, arr_a= None, arr_b= None): # Constructor
+    Attributes
+    ----------
+    arr_a : None
+        Creating a matrix using numpy package.
+    arr_b: None
+        Creating a matrix using numpy package.
+
+    Methods
+    -------
+    add(dim=int)
+        Returns addition of two arrays of same dimension.
+    sub(dim=int)
+        Returns subtraction of two arrays of same dimension.
+    multi(dim)
+        Returns multiplication of two arrays of same dimension.
+    """
+    
+    def __init__(self, arr_a= None, arr_b= None):
+        """
+        parameters
+        ----------
+        arr_a: None
+            Creating a matrix using numpy package.
+        arr_b: None
+            Creating a matrix using numpy package.
+        """
+
         self.arr_a= np.array(arr_a)
         self.arr_b= np.array(arr_b)
         self.result= None
     
     def add(self, dim):
-        """ Returns addition of two arrays of same dimension """
+        """
+        Perform addition on matrices and return the result of the 
+        addtion of matrices.
+
+        parameters
+        ----------
+        dim : int
+            The dimension of  matrix, for use by the addend or augend.
+        """
+
         self.arr_a= matrix(dim, dim) 
         self.arr_b= matrix(dim, dim)
         self.result= np.add(self.arr_a, self.arr_b)
         return self.result
     
     def sub(self, dim):
-        """ Returns subtraction of two arrays of same dimension """
+        """ 
+        Perform subraction on matrices and return the result of the 
+        subtraction of matrices.
+
+        parameters
+        ----------
+        dim : int
+            The dimension of  matrix, for use by both matrix to be 
+            subtracted from another matrix.
+        """
+
         self.arr_a= matrix(dim, dim) 
         self.arr_b= matrix(dim, dim)
         self.result= np.subtract(self.arr_a, self.arr_b)
         return self.result
     
     def multi(self, dim):
-        """ Returns multiplication of two arrays of same dimension """
+        """ 
+        Perform Multiplication on matrices and return the result of the 
+        multiplication of matrices.
+
+        parameters
+        ----------
+        dim : int
+            The dimension of  matrix, for use by both matrix to be 
+            multiplied.
+        """
+
         self.arr_a= matrix(dim, dim) 
         self.arr_b= matrix(dim, dim)
         self.result= np.multiply(self.arr_a, self.arr_b)
