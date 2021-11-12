a970bbf82f22924952a20a2c5e681f188497f849
s.senthilnathan <libertariansenthil@gmail.com>
2021-11-13 00:47:51 +0530

    Comment changes

--- matrix_project/main.py
+++ matrix_project/main.py
@@ -1,7 +1,7 @@
 #  Status: active
-# Creating a cmd line interface for application.
-#  This module gets options for the matrix calculation and finally displays the result.
-
+"""
+This is the CLI(Command Line Interface) for matrix calculator.
+"""
 
 from matrix import arithmetic, formula
 
@@ -24,51 +24,52 @@ while True:
     else:
          break
 
-# Processing the input to the option.
-# We can give the No. of dimension needed in non-programming side
+# Creating objects for arithmetic and formula classes
 oper_arithmetic = arithmetic.arithmetic()
 operation_for = formula.formula()
-if option == 1:
-    print("addition") #test code
+
+# Processing the input from the option.
+if option == 1:     # Addition
+    print("addition") 
     ip= input("Enter the No. of dimensions:")
     if ip == "quit":
         print("Exited")
     else:
         print(oper_arithmetic.add(eval(ip)))
         input("Press Enter key to exit... ")
-elif option ==2:
-    print("Subtraction") #test code
+elif option == 2:   # Subtraction
+    print("Subtraction") 
     ip= input("Enter the No. of dimensions:")
     if ip == "quit":
         print("Exited")
     else:
         print(oper_arithmetic.sub(eval(ip)))
         input("Press Enter key to exit... ")
-elif option == 3:
-    print("Multiplication") #test code
+elif option == 3:   # Multiplication
+    print("Multiplication") 
     ip= input("Enter the No. of dimensions:")
     if ip == "quit":
         print("Exited")
     else:
         print(oper_arithmetic.multi(eval(ip)))
         input("Press Enter key to exit... ")
-elif option == 4:
-    print("square of A") #test code
+elif option == 4:   # Squaring
+    print("square of A") 
     ip= input("Enter the No. of dimensions:")
     if ip == "quit":
         print("Exited")
     else:
         print(operation_for.square(eval(ip)))
         input("Press Enter key to exit... ")
-elif option == 5:
-    print("transpose")   # test code
+elif option == 5:   # Transpose
+    print("transpose")   
     ip= input("Enter the No. of dimensions:")
     if ip == "quit":
         print("Exited")
     else:
         print(operation_for.trans(eval(ip)))
         input("Press Enter key to exit... ")
-elif option == 6:
+elif option == 6:   # Quit
     print("Exited") 
 else: 
     print("Invalid input") 
