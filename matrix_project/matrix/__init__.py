63ad246caec0c52e32b93176905938c1d89726e7
s.senthilnathan <libertariansenthil@gmail.com>
2021-11-13 11:20:21 +0530

    added path to this package-> matrix(package)

--- matrix_project/matrix/__init__.py
+++ matrix_project/matrix/__init__.py
@@ -4,3 +4,8 @@
 # Current version of the project.
 __version__ = "0.1.1" # Lastly updated on 27th oct 2021.
 __author__ = ["S.Ramachandran","G.Deepika","S.Senthilnathan", "G.Sakthi swetha", "S.Thanushri"]
+
+import os, sys
+
+file_path= "matrix_project\\matrix\\"
+sys.path.append(os.path.dirname(file_path))
